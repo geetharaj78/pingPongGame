138392ee1d0c92e486634b3062c2576234fed840
geetharaj78 <ch.geethaR78@gmail.com>
2025-06-18 21:39:05 +0530

    Delete quizgame directory

--- quizgame/question_model.py
+++
@@ -1,6 +0,0 @@
-class Question:
-    def __init__(self,text,answer): #we have created our question class from scratch
-        self.text = text
-        self.answer = answer
-
-#now we create a question_bank object by using the Question class.
