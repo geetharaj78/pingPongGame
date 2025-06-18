6283b90e013f7f759bb4c4302bb4323cdf5b73bc
geetharaj78 <ch.geethaR78@gmail.com>
2025-06-18 21:38:32 +0530

    Delete oop directory

--- oop/main1.py
+++
@@ -1,8 +0,0 @@
-class User:
-    pass
-
-user_1 = User()  #we are creating an object user_1 for the class User()
-user_1.id = "001" #id nd the below all attribute now exists in user_1 object. 
-user_1.username = "angela"
-
-print(user_1.username)
