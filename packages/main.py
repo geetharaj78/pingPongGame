c7c763a9a97437d2db3dda5e42d92c47c0e67be1
geetharaj78 <ch.geethaR78@gmail.com>
2025-06-18 21:38:45 +0530

    Delete packages directory

--- packages/main.py
+++
@@ -1,6 +0,0 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("characteristics of the person",["lazy","funny","witty"])
-table.add_column("good/bad",["good","bad","good"])
-table.align = "l"
-print(table)
